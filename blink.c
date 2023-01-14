2acfa798c303e0fd77e4678f28967e65e1fd07c4
SalazarBryan <93726639+SalazarBryan@users.noreply.github.com>
2023-01-13 23:24:39 -0600

    final

--- blink.c
+++ blink.c
@@ -1,75 +1,67 @@
-//#include <pic14/pic12f683.h>
+#include <stdio.h>
 #include <pic14/pic12f675.h>
-//#define _LEGACY_HEADERS
-//#define _MCLRE_OFF 0x3FDF
-//#define _WDTE_OFF 0x3FF7  
 
-#define LOOP?WAIT?LIMIT 100 
-
-// Inicialización de perifericos y MCU
-// Configuración ** deshabilitar reset y watchdog timer con macros
+/* Inicialización de perifericos y MCU
+Configuración ** deshabilitar reset y watchdog timer con macros */ 
 typedef unsigned int word;
 word __at 0x2007 __CONFIG =  (_MCLRE_OFF & _WDTE_OFF & _BODEN_OFF); 
 
 void delay(unsigned inttiempo); // Acá se define la función del tiempo de espera
 
-void main(void) // Función principal para la ejecución de acciones
+void main(void) 				// Función principal 
 {
-	// ** Establecimiento de las condiciones en registros
-	TRISIO = 0b00000000; // Poner todos los pines como salidas 
-	GPIO = 0b00100000;   // Poner pines en bajo; terminales en 0
-	ANSEL = 0;			 // Canales analogicos como E/S digitales
-	CMCON = 0x07;			 // Apagar comparadores
-	unsigned int time1 = 400;   // Tiempo de espera antes apagar
-	unsigned int time = 100;
+	// **Condiciones en registros
+	TRISIO = 0b00000000; 		// pines como salidas 
+	GPIO = 0b00100000;   		// terminales en 0
+	ANSEL = 0;			// Canales analogicos como Dig
+	CMCON = 0x07;			    
+	unsigned int time1 = 150;   // Tiempo de espera antes de apagar la configuración de los leds
 
-	// Loop forever
+	
 	while (1)
 	{	
-		unsigned int counter = 1;	
-		while (1){
-			  if (GP5 == 0x01){ // Hacemos el contador para que retire numero aleatorio
+		unsigned int counter = 1;	// se define contador
+		while (1)
+		{
+			if (GP5 == 0x01){     // contador para que sostenga un  número 
 				counter ++;
-				if (counter == 6){
-					counter = 1;
+				if (counter == 7){  //número aleatorio entre 1 y 6
+					counter = 0;
 					counter ++;
-					GP0 = ~GP0;
-					GP1 = ~GP1;
-					GP2 = ~GP2;
-					GP4 = ~GP4;
 				}	
-			  } else { if (GP5 == 0x00){
+			} else { if (GP5 == 0x00){ // Al presionar bottón se configura la salida de los leds
+				if (counter == 1){      
+					GP1 = ~GP1;
+					delay(time1);		// Llamada a condición de espera
+					GPIO = 0x20;
+					
+					//// Para mostrar el dos, tres, cuatro, cinco y seis en la configuración de leds
 				
-				if (counter == 1){
-					GP1 = ~GP1;
-					delay(time1);
-					GPIO = 0x20;
-					
-				} if (counter == 2){
+				} if (counter == 2){    
 					GP2 = ~GP2;
 					delay(time1);
 					GPIO = 0x20;
 					
-				} if (counter == 3){
+				} if (counter == 3){   
 					GP2 = ~GP2;
 					GP1 = ~GP1;
 					delay(time1);
 					GPIO = 0x20;
 					
-				} if (counter == 4){
+				} if (counter == 4){  
 					GP0 = ~GP0;
 					GP2 = ~GP2;
 					delay(time1);
 					GPIO = 0x20;
 					
-				} if (counter == 5){
+				} if (counter == 5){   
 					GP0 = ~GP0;
 					GP1 = ~GP1;
 					GP2 = ~GP2;
 					delay(time1);
 					GPIO = 0x20;
 					
-				} if (counter == 6){
+				} if (counter == 6){    
 					GP0 = ~GP0;
 					GP2 = ~GP2;
 					GP4 = ~GP4;
@@ -91,4 +83,5 @@ void delay(unsigned int tiempo) // Función para imprimir delay
 		for (j = 0; j < 1275; j++)
 			;
 }
+
 
